3d8ef918097667f4f75158b0819d44fa04036f00
MrLenin <909621+MrLenin@users.noreply.github.com>
2024-10-27 22:05:29 -0400

    build.sh: Automatically timestamp pseudofile based on git epoch time. Stylistic improvements.

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-FW_VARIANT="G-010S-P"
+FW_VARIANT="G-010S-P"   # Image variant
 
-blk_size=262144
-pad_end=4226953
-img_end=4259840
+BLOCK_SIZE=262144       # Image block size
+ZZ_END=4226953          # End of '00' padding
+FF_END=4259840          # End of 'FF' padding
 
 sha256() {
 	{ [ -n "$1" ] &&  sha256sum "$1" || sha256sum; } | awk '{print $1}'
@@ -52,25 +52,40 @@ version = "${FW_VERSION}"
 revision = "${FW_REVISION}"
 8311VER
 
-mkimage -A mips -O linux -T kernel -C lzma -a 0X80002000 -e 0X80002000 -n SFP_7.5.3 -d source/kernel_g-010s-p.lzma build/uImage_g-010s-p
+mkimage -A mips -O linux -T kernel -C lzma -a 0X80002000 -e 0X80002000 -n SFP_7.5.3 -d ./source/kernel_g-010s-p.lzma ./build/uImage_g-010s-p
+touch -d "@$GIT_EPOCH" ./source/kernel_g-010s-p.lzma ./build/uImage_g-010s-p
 
-unsquashfs -exclude-file excludes -pf build/8311_g-010s-p.pseudo source/rootfs_g-010s-p.sqsh
-mksquashfs - build/8311.squashfs -b 262144 -comp xz -pf build/8311_g-010s-p.pseudo -pf pseudofile
+sed -r "s/@TIMESTAMP/$GIT_EPOCH/g" ./pseudofile >./build/pseudofile
 
-cat build/uImage_g-010s-p build/8311.squashfs > build/alcatel-g010sp_8311.img
+# When passing unsquashfs '-pf -' WSL creates a pseudofile '-' which aside from creating other
+# problems, defeats the entire point of piping...
+case $(uname -r) in
+  *WSL*)
+    unsquashfs -exclude-file excludes -pf ./build/8311_g-010s-p.pseudo ./source/rootfs_g-010s-p.sqsh
+    mksquashfs - ./build/8311.squashfs -b 262144 -comp xz -pf ./build/8311_g-010s-p.pseudo -pf ./build/pseudofile -mkfs-time "$GIT_EPOCH"
+    ;;
+  *)
+    unsquashfs -exclude-file excludes -pf - ./source/rootfs_g-010s-p.sqsh | \
+    mksquashfs - ./build/8311.squashfs -b 262144 -comp xz -pf - -pf ./build/pseudofile -mkfs-time "$GIT_EPOCH"
+    ;;
+esac
 
-img_size=$(stat -c%s build/alcatel-g010sp_8311.img)
+cat ./build/uImage_g-010s-p ./build/8311.squashfs >build/alcatel-g010sp_8311.img
 
-padding=$((pad_end-img_size)) bs=${blk_size} nblocks=$((padding/bs)) rest=$((padding%bs))
+IMG_SIZE=$(stat -c%s ./build/alcatel-g010sp_8311.img)
+
+PADDING=$((ZZ_END-IMG_SIZE)) NUM_BLOCKS=$((PADDING/BLOCK_SIZE)) REST=$((PADDING%BLOCK_SIZE))
 {
-  dd if=/dev/zero bs=$bs count=$nblocks
-  dd if=/dev/zero bs=$rest count=1
-} 2>/dev/null >> build/alcatel-g010sp_8311.img
+  dd if=/dev/zero bs=$BLOCK_SIZE count=$NUM_BLOCKS
+  dd if=/dev/zero bs=$REST count=1
+} 2>/dev/null >>build/alcatel-g010sp_8311.img
 
-padding=$((img_end-pad_end)) bs=${blk_size} nblocks=$((padding/bs)) rest=$((padding%bs))
+PADDING=$((FF_END-ZZ_END)) NUM_BLOCKS=$((PADDING/BLOCK_SIZE)) REST=$((PADDING%BLOCK_SIZE))
 {
-  dd if=/dev/zero bs=$bs count=$nblocks | tr "\000" "\377" >> build/alcatel-g010sp_8311.img
-  dd if=/dev/zero bs=$rest count=1 | tr "\000" "\377" >> build/alcatel-g010sp_8311.img
-} 2>/dev/null >> build/alcatel-g010sp_8311.img
+  dd if=/dev/zero bs=$BLOCK_SIZE count=$NUM_BLOCKS | tr "\000" "\377"
+  dd if=/dev/zero bs=$REST count=1 | tr "\000" "\377"
+} 2>/dev/null >>build/alcatel-g010sp_8311.img
 
-cp build/alcatel-g010sp_8311.img out/alcatel-g010sp_8311.img
+touch -d "@$GIT_EPOCH" ./build/alcatel-g010sp_8311.img
+
+mv ./build/alcatel-g010sp_8311.img ./out/alcatel-g010sp_8311.img
